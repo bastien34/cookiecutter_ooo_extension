2119c866aa02a6bb29914615d4fc0576686c424b
Bastien Roques <bastien.roques1@gmail.com>
2019-03-23 17:57:06 +0100

    Create config.xcs ok - needs to take values from odt

--- tools/create_config_xcs.py
+++ tools/create_config_xcs.py
@@ -3,146 +3,60 @@
 import os
 import logging
 import xml.etree.ElementTree as ET
-from helper import Elem, ElemProp, ElemPropLoc
+from _elementtree import Element
+from helper import (ElemProp, Elem)
 
+extension_filename = "{{cookiecutter.extension_name}}-{{cookiecutter.extension_version}}.oxt"
+extension_id = "com.pwd.myextension"
+package_name = "{{cookiecutter.package_name}}"
+xml_file = f"{package_name}_config.xcs"  # To be adjusted in the right location
+locale = {"xml:lang": "fr"}
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('create_config_xcu')
-
-extension_id = 'com.pwd.myextension'
-xml_file = 'OptionsDialogDev.xcu'
+logger = logging.getLogger("Now, it's time to create the %s file!\n" % xml_file)
 
 
-class ElemLeaf(Elem):
-    def __init__(self, attrs):
-        if "Id" not in attrs:
-            attrs["Id"] = attrs["Name"]
-        attrs["OptionsPage"] = "%origin%/dialogs/optionsdialog.xdl"
-        attrs["EventHandlerService"] = extension_id
-        super().__init__("node",
-                         {'oor:name': attrs.pop("Name"), "oor:op": "fuse"})
-        for key, val in attrs.items():
-            if key == "Label":
-                self.append(ElemPropLoc(key, val))
-            else:
-                self.append(ElemProp(key, val))
-
-
-class ElemNode(Elem):
-    def __init__(self, attrs, *, leaves=None):
-        super().__init__("node",
-                         {'oor:name': attrs.pop("Name"), "oor:op": "fuse"})
-        for key, val in attrs.items():
-            if key == "Label":
-                self.append(ElemPropLoc(key, val))
-            else:
-                self.append(ElemProp(key, val))
-        if leaves is not None:
-            self.append(Elem("node", {'oor:name': "Leaves"}))
-            for leaf in leaves:
-                self[-1].append(leaf)
-
-
-class ElemOrderdNode(Elem):
-    def __init__(self, name, index=None):
-        super().__init__("node", {'oor:name': name, "oor:op": "fuse"})
-        if index is not None:
-            self.append(ElemProp("Index", index))
-
-
-class ElemModule(Elem):
-    def __init__(self, name,
-                 nodes):
-        super().__init__("node", {'oor:name': name, "oor:op": "fuse"})
-        self.append(Elem("node", {'oor:name': "Nodes"}))
-        for node in nodes:
-            self[-1].append(node)
-
-
-class ElemSingleOption(Elem):
-    def __init__(self, name):
-        super().__init__("node", {'oor:name': name, "oor:op": "fuse"})
-        self.append(ElemProp("Hide", "true"))
-
-
-class ElemOptionsPage(Elem):
-    def __init__(self, name, *, hide=None, singleoptions=None):
-        super().__init__("node", {'oor:name': name, "oor:op": "fuse"})
-        if hide is not None:
-            self.append(ElemProp("Hide", "true"))
-        if singleoptions is not None:
-            self.append(Elem("node", {'oor:name': "Options"}))
-            for singleoption in singleoptions:
-                self[-1].append(
-                    singleoption)
-
-
-class ElemOptionsGroup(Elem):
-    def __init__(self, name, *, hide=None, optionspages=None):
-        super().__init__("node", {'oor:name': name, "oor:op": "fuse"})
-        if hide is not None:
-            self.append(ElemProp("Hide", "true"))
-        if optionspages is not None:
-            self.append(Elem("node", {'oor:name': "Pages"}))
-            for optionspage in optionspages:
-                self[-1].append(
-                    optionspage)
-
-
-def create_config_xcu():
+def create_config_xcs():
     """
     Creation of OptionsDialog.xcu
     """
     path = os.path.dirname(os.path.realpath(__file__))
-    logger.info('Creating xcu in: %s' % path)
+    logger.info("Creating xcs in: %s" % path)
 
-    with open(xml_file, "w", encoding="utf-8") as f:
-        root = Elem("oor:component-data",
-                    {"oor:name": "OptionsDialog",
-                     "oor:package": "org.openoffice.Office",
-                     "xmlns:oor": "http://openoffice.org/2001/registry",
-                     # "xmlns:xs": "http://www.w3.org/2001/XMLSchema",
-                     # "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
-                     })
+    root = Element("oor:component-schema",
+                   {"xmlns:oor": "http://openoffice.org/2001/registry",
+                    "xmlns:xs": "http://www.w3.org/2001/XMLSchema",
+                    "oor:name": "ExtensionData",
+                    "oor:package": "{{cookiecutter.package_name}}",
+                    "xml:lang": "en-US"})
 
-        root.append(Elem("node", {'oor:name': "Nodes"}))
+    with open(xml_file, "w", encoding="UTF-8") as f:
+        template = ET.SubElement(root, "templates")
+        group = ET.SubElement(template, 'group', {'oor:name': "Production"})
+        inf = ET.SubElement(group, 'info')
+        inf.append(Elem('desc', {'text': 'Values for production mode'}))
 
-        leaf = ElemLeaf({"Name": extension_id,
-                         "Label": {"en-US": "Maximum Paper Size"}})
-        name = "{}.Node1".format(extension_id)
-        node = ElemNode({"Name": name,
-                         "Label": {"en-US": "Extensions Example", },
-                         "AllModules": "false"},
-                        leaves=(leaf,))
-        root[-1].append(node)
-        #
-        # root.append(Elem("node", {'oor:name': "Modules"}))
-        # orderednode = ElemOrderdNode(
-        #     name)
-        # node = ElemModeule("com.sun.star.text.TextDocument", (
-        #     orderednode,))
-        # root[-1].append(node)
-        #
-        # root.append(Elem("node", {
-        #     'oor:name': "OptionsDialogGroups"}))
-        # security = ElemOptionsPage("Security",
-        #                            hide=True)
-        # memory = ElemOptionsPage("Memory",
-        #                          hide=True)
-        # productname = ElemOptionsGroup("ProductName", optionspages=(security,
-        #                                                             memory))
-        # root[-1].append(
-        #     productname)
-        # internet = ElemOptionsGroup("Internet",
-        #                             hide=True)
-        # root[-1].append(
-        #     internet)
-        #
+        # Here, iteration on options list
+        group.append(ElemProp('test_mode', 'un petit texte',
+                              {'oor:type': "xs.boolean"}))
+
+        # Manage default values
+        defaults = ET.SubElement(group, 'group', {'oor:name': 'Defaults'})
+
+        # Iteration on default values
+        defaults.append(ElemProp('test_mode', '0', {'oor:type': 'xs:boolean'}))
+
+        # Component
+        cmp = ET.SubElement(root, 'component')
+        leaves = ET.SubElement(cmp, 'Leaves')
+        leaves.append(
+            Elem('node-ref', {'oor:name': "{{cookiecutter.extension_name}}",
+                              'oor:node': "Production"}))
+
         tree = ET.ElementTree(root)
         tree.write(f.name, "utf-8", True)
-
-    logger.info('%s has been created. Thanks for support !' % xml_file)
+    logger.info("%s has been created.\nThanks for support !" % xml_file)
 
 
 if __name__ == "__main__":
-    create_config_xcu()
+    create_config_xcs()
--- tools/helper.py
+++ tools/helper.py
@@ -8,18 +8,11 @@ from _elementtree import Element
 class Elem(ET.Element):
     """
     xml.etree.ElementTree.Element
-    tag: tag name
-    attrib: dict with attributes
-    kwargs: look at the parent class
     """
 
     def __init__(self, tag, attrib={}, **kwargs):
-        """This init is a copy of parent class ET.Element"""
-        print(tag, attrib)
         if "text" in kwargs:
-            txt = kwargs["text"]
-            print('text in kwargs %s' % txt)
-            del kwargs["text"]
+            txt = kwargs.pop("text")
             super().__init__(tag, attrib, **kwargs)
             self._text(txt)
         else:
@@ -30,13 +23,14 @@ class Elem(ET.Element):
 
 
 class ElemProp(Element):
-    def __init__(self, name, txt):
-        super().__init__("prop", {'oor:name': name})
+    def __init__(self, name, txt, attrib={}):
+        attrib.update({'oor:name': name})
+        super().__init__("prop", attrib)
         self.append(Elem("value", text=txt))
 
 
-class ElemPropLoc(Elem):
-    def __init__(self, name, langs):
-        super().__init__("prop", {'oor:name': name})
+class ElemPropLoc(ElemProp):
+    def __init__(self, name, langs, attrib={}):
+        super().__init__(name, attrib)
         for lang, value in langs.items():
             self.append(Elem("value", {"xml:lang": lang}, text=value))
