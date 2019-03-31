580afdb244a61725612c82c600d75e5a75ff2e7c
Bastien Roques <bastien.roques1@gmail.com>
2019-03-31 20:24:20 +0200

    Extension created from odt with extra_context - no addons yet

--- tools/starter.py
+++ tools/starter.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 import logging.config
 import yaml
+from cookiecutter.main import cookiecutter
+
+# TODO: group these 3 functions in one module
 from create_addon_ui import (create_addon,
                              test_functions)
 from create_config_xcs import (create_config_xcs,
@@ -9,9 +12,46 @@ from create_config_xcs import (create_config_xcs,
 from create_dialog import create_dialog
 
 
-"""
-This starter needs a Environ.
-"""
+EXTENSION_DESCRIPTION = (
+    'extension_name',
+    'extension_label',
+    'extension_version',
+    'packages_name',
+    'company_name',
+    'author_name',
+    'update_url',
+    'publisher_url',
+    'image_name',
+)
+output_dir = '/home/bastien/tmp'
+repo = "https://github.com/bastien34/cookiecutter_ooo_extension"
+
+
+def test_launcher(*args):
+    """
+    Launcher... For now connected to `extension_manager.odt`
+
+    Instead of creating a yaml file `.cookiecuttercc`, it seems
+    wiser to send a dict as extra_context.
+
+    """
+    doc = XSCRIPTCONTEXT.getDocument()
+    tb = doc.getTextTables().getByName('description_table')
+    description_data = tb.getDataArray()
+    tb = doc.getTextTables().getByName('function_table')
+    function_data = tb.getDataArray()
+    tb = doc.getTextTables().getByName('option_table')
+    option_data = tb.getDataArray()
+
+    extra_context = {}
+    [extra_context.update({r[0]: r[1]}) for r in description_data]
+
+    # Before generating the project, we must create our config files.
+
+    cookiecutter('cookiecutter-django', no_input=True,
+                 extra_context={},
+                 output_dir=output_dir
+                 )
 
 
 if __name__ == "__main__":
