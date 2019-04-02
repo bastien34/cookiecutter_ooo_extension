3364c8ab3e6c845bf5bdc6ec944c6d715ce8db79
Bastien Roques <bastien.roques1@gmail.com>
2019-04-02 14:23:18 +0200

    todos

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,6 +3,10 @@
 
 import logging
 
+# TODO: assure that name vars or functions are no capitalized and without blank
+#  space.
+# TODO: module.py : adapt environ and starter feature
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('pre_gen_project')
 
